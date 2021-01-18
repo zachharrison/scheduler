5e5551d8c8765558957ad4a7b28676c976fe01a8
Zach Harrison <zachharrison@Zachs-MacBook-Pro.local>
2021-01-18 13:57:15 -0800

    Added classes and props to button component and ensured that all tests pass in storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
